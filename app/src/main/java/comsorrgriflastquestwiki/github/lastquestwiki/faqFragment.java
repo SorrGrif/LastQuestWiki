7cf5a81de4cc36db21ef1515437573b29a3b46cb
John Cano <john.cano@stclairconnect.ca>
2016-11-20 11:32:56 -0500

    created the faq java class, and implented the getter and setter, #3

--- app/src/main/java/comsorrgriflastquestwiki/github/lastquestwiki/faqFragment.java
+++ app/src/main/java/comsorrgriflastquestwiki/github/lastquestwiki/faqFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
@@ -26,6 +28,8 @@ public class faqFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView faqFragment;
+    ListView list;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,7 +68,8 @@ public class faqFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faq, container, false);
+        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
---
+++ app/src/main/java/comsorrgriflastquestwiki/github/lastquestwiki/questionItem.java
@@ -0,0 +1,8 @@
+package comsorrgriflastquestwiki.github.lastquestwiki;
+
+/**
+ * Created by Reishin on 2016-11-20.
+ */
+
+public class questionItem {
+}
