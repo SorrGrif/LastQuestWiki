9e3224931edc958eed3784079b7f210b2af23750
John Cano <john.cano@stclairconnect.ca>
2016-11-28 11:18:49 -0500

    created the viewpager for the java file #2

--- app/src/main/java/comsorrgriflastquestwiki/github/lastquestwiki/tipsFragment.java
+++ app/src/main/java/comsorrgriflastquestwiki/github/lastquestwiki/tipsFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +26,12 @@ public class tipsFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,7 +70,11 @@ public class tipsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tips, container, false);
+
+         View view =  inflater.inflate(R.layout.fragment_tips, container, false);
+        mSectionsPagerAdapter = new SectionPagerAdapter(getActivity().getSupportFragmentManager());
+        mViewPager = (ViewPager) view.findViewById(R.id.tipcontent);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -104,5 +114,14 @@ public class tipsFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public class SectionPagerAdapter extends FragmentPagerAdapter{
+        public SectionPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+        public Fragment getItem(int position){
+
+        }
     }
 }
